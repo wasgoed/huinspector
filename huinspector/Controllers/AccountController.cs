2d3103a0e7fe2f1487fd13c2aeb918bff38d9e64
Edwin Miltenburg <mamoru@edwinmiltenburg.nl>
2015-03-12 13:53:54 +0100

    Removed remaining lockout

--- huinspector/Controllers/AccountController.cs
+++ huinspector/Controllers/AccountController.cs
@@ -73,15 +73,12 @@ namespace huinspector.Controllers
                 return View(model);
             }
 
-            // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
                     return RedirectToLocal(returnUrl);
-                case SignInStatus.LockedOut:
-                    return View("Lockout");
                 case SignInStatus.Failure:
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
