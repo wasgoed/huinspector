33c76635e5fa98b4742df4941785babf853fc300
Edwin Miltenburg <mamoru@edwinmiltenburg.nl>
2015-03-23 22:13:18 +0100

    Added User(Type) Controllers and Account/Identity Models

---
+++ huinspector/Controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using huinspector.Models;
+
+namespace huinspector.Controllers
+{
+    public class UserController : Controller
+    {
+        private HUInspectorEntities1 db = new HUInspectorEntities1();
+
+        // GET: /User/
+        public ActionResult Index()
+        {
+            var user = db.User.Include(u => u.UserType);
+            return View(user.ToList());
+        }
+
+        // GET: /User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: /User/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserTypeId = new SelectList(db.UserType, "Id", "Name");
+            return View();
+        }
+
+        // POST: /User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,Insertion,LastName,Mail,Password,UserTypeId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserTypeId = new SelectList(db.UserType, "Id", "Name", user.UserTypeId);
+            return View(user);
+        }
+
+        // GET: /User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserTypeId = new SelectList(db.UserType, "Id", "Name", user.UserTypeId);
+            return View(user);
+        }
+
+        // POST: /User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,Insertion,LastName,Mail,Password,UserTypeId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserTypeId = new SelectList(db.UserType, "Id", "Name", user.UserTypeId);
+            return View(user);
+        }
+
+        // GET: /User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.User.Find(id);
+            db.User.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ huinspector/Controllers/UserTypeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using huinspector.Models;
+
+namespace huinspector.Controllers
+{
+    public class UserTypeController : Controller
+    {
+        private HUInspectorEntities1 db = new HUInspectorEntities1();
+
+        // GET: /UserType/
+        public ActionResult Index()
+        {
+            return View(db.UserType.ToList());
+        }
+
+        // GET: /UserType/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserType usertype = db.UserType.Find(id);
+            if (usertype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usertype);
+        }
+
+        // GET: /UserType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /UserType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] UserType usertype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserType.Add(usertype);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usertype);
+        }
+
+        // GET: /UserType/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserType usertype = db.UserType.Find(id);
+            if (usertype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usertype);
+        }
+
+        // POST: /UserType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] UserType usertype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usertype).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usertype);
+        }
+
+        // GET: /UserType/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserType usertype = db.UserType.Find(id);
+            if (usertype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usertype);
+        }
+
+        // POST: /UserType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserType usertype = db.UserType.Find(id);
+            db.UserType.Remove(usertype);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
